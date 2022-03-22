58b21b6db1d20e6243e38309fef04a6510a57bde
a.rebizov <a.rebizov@elama.ru>
2022-03-22 09:27:43 +0300

    Окружение

---
+++ conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: es or ru")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(
+            'C:\\Users\\a.rebizov\\PycharmProjects\\pythonProject\\pythonProject1\\chromedriver\\chromedriver.exe')
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(
+            'C:\\Users\\a.rebizov\\PycharmProjects\\pythonProject\\pythonProject1\\chromedriver\\geckodriver.exe')
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
