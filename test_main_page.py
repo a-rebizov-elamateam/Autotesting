cec0ab2e559392ec11d7fc3364150c25b3872979
a.rebizov <a.rebizov@elama.ru>
2022-03-23 12:22:12 +0300

    added go to LoginPage + cod comment

--- test_main_page.py
+++ test_main_page.py
@@ -1,19 +1,22 @@
+import time
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
+    link = "https://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
-    login_page.should_be_login_page()
+    page.go_to_login_page()  # переход на страницу LoginPage
+    time.sleep(5)
+    # login_page = page.go_to_login_page() #  в переменную записываем возвращенный LoginPage из go_to_login_page(self)
+    # login_page.should_be_login_page() #  проверяем все элементы страницы LoginPage
+    login_page = LoginPage(browser, browser.current_url)  # в переменную записываем LoginPage
+    login_page.should_be_login_page()  # проверяем все элементы страницы LoginPage
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "https://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-print('111111111111111111')
